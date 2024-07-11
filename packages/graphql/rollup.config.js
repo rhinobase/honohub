e042abc41cfd516923c048affa480184e56b4a7b
Aditya Mathur <mathuraditya724@gmail.com>
2024-07-11 12:01:51 +0530

    fix(gql): corrected validation and styling

--- packages/graphql/rollup.config.js
+++ packages/graphql/rollup.config.js
@@ -16,7 +16,7 @@ module.exports = withNx(
     // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
     input: {
       index: "./src/index.ts",
-      playground: "./src/playground.tsx",
+      playground: "./src/playground/index.tsx",
     },
     plugins: [
       svg({
---
+++ packages/graphql/src/playground/index.tsx
@@ -0,0 +1,17 @@
+import { createGraphiQLFetcher } from "@graphiql/toolkit";
+import GraphiQL from "graphiql";
+import "graphiql/graphiql.min.css";
+import "./page.css";
+import { useMemo } from "react";
+
+export type GraphQLEditorProps = {
+  endpoint: string;
+};
+
+export function GraphQLEditor(props: GraphQLEditorProps) {
+  const fetcher = useMemo(
+    () => createGraphiQLFetcher({ url: props.endpoint }),
+    [props.endpoint],
+  );
+  return <GraphiQL fetcher={fetcher} />;
+}
