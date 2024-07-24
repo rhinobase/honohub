d890e04188f8ef7fcc7ca5b755a55252ddd5798c
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2024-07-24 18:00:43 +0530

    fix(gql): minor change

--- packages/graphql/src/validation.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const graphQLBodyValidation = z.object({
-  operationName: z.string().optional(),
-  query: z.string(),
-  variables: z.record(z.string(), z.any()).optional(),
-});
