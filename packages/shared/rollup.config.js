223a89bf091eab6086e9a24af95bd239cbfdf76a
Aditya Mathur <mathuraditya724@gmail.com>
2024-09-11 09:41:20 +0530

    feat(shared): init commit

---
+++ packages/shared/rollup.config.js
@@ -0,0 +1,30 @@
+const { withNx } = require("@nx/rollup/with-nx");
+const url = require("@rollup/plugin-url");
+const svg = require("@svgr/rollup");
+const terser = require("@rollup/plugin-terser");
+
+module.exports = withNx(
+  {
+    main: "./src/index.ts",
+    outputPath: "../../dist/packages/shared",
+    tsConfig: "./tsconfig.lib.json",
+    compiler: "swc",
+    external: ["react", "react-dom", "react/jsx-runtime"],
+    format: ["esm"],
+    assets: [{ input: ".", output: ".", glob: "README.md" }],
+  },
+  {
+    // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
+    plugins: [
+      svg({
+        svgo: false,
+        titleProp: true,
+        ref: true,
+      }),
+      url({
+        limit: 10000, // 10kB
+      }),
+      terser(),
+    ],
+  },
+);
