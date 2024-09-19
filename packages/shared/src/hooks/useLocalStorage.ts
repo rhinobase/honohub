719bbb57863b81a3545ed5bf5caa003231b51b99
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2024-09-19 13:09:14 +0530

    feat(shared): added useLocalStorage hook

---
+++ packages/shared/src/hooks/useLocalStorage.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export type useLocalStorage<T> = {
+  storageKey: string;
+  defaultValue?: T;
+};
+
+export function useLocalStorage<T>(props: useLocalStorage<T>) {
+  const [state, setState] = useState<T>();
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Only need to run on state change
+  useEffect(() => {
+    if (state == null)
+      setState(
+        (localStorage.getItem(props.storageKey) as T) ?? props.defaultValue,
+      );
+    else localStorage.setItem(props.storageKey, String(state));
+  }, [state]);
+
+  return [state, setState] as const;
+}
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,6 +2,7 @@ export * from "./ActionSelect";
 export * from "./ErrorComponent";
 export * from "./FormFooter";
 export * from "./HighlightMatches";
+export * from "./hooks";
 export * from "./LoadingComponent";
 export * from "./PageHeader";
 export * from "./rowSelectionColumn";
