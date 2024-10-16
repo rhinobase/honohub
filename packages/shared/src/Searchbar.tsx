2274a38ded30d9a0ef8cf6cdfdebaf6123544281
Aditya Mathur <mathuraditya724@gmail.com>
2024-10-16 10:25:04 +0530

    feat(shared): added local mode in searchbar

--- packages/shared/src/Searchbar.tsx
+++ packages/shared/src/Searchbar.tsx
@@ -4,19 +4,26 @@ import { useDebounce } from "@uidotdev/usehooks";
 import { useEffect, useState } from "react";
 
 export type Searchbar = SearchField &
-  Omit<useQueryParams, "searchParams"> & {
+  Omit<useQueryParams, "searchParams" | "onChange"> & {
     searchParams: URLSearchParams;
     paramName?: string;
-  };
+  } & (
+    | ({ local?: false } & Pick<useQueryParams, "onChange">)
+    | {
+        local: true;
+        onChange: (search?: string) => void;
+      }
+  );
 
 export function Searchbar({
+  local,
   onChange,
   pathname,
   searchParams,
   paramName = "search",
   ...props
 }: Searchbar) {
-  const defaultValue = searchParams.get(paramName) ?? undefined;
+  const defaultValue = !local ? searchParams.get(paramName) : null;
 
   const [value, setValue] = useState(defaultValue);
   const search = useDebounce(value?.trim(), 150);
@@ -29,9 +36,16 @@ export function Searchbar({
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: need to push new url only when search changes
   useEffect(() => {
-    if (search) setQueryParams({ search });
+    if (local) onChange(search);
+    else if (search) setQueryParams({ search });
     else onChange(pathname);
   }, [search]);
 
-  return <SearchField {...props} value={value} onValueChange={setValue} />;
+  return (
+    <SearchField
+      {...props}
+      value={value ?? undefined}
+      onValueChange={setValue}
+    />
+  );
 }
