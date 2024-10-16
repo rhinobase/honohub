92b3dd32c040b01fe8d4c5997d132f82ffaaa545
Aditya Mathur <mathuraditya724@gmail.com>
2024-10-16 12:30:13 +0530

    fix(shared): updated searchbar types

--- packages/shared/src/Searchbar.tsx
+++ packages/shared/src/Searchbar.tsx
@@ -4,41 +4,53 @@ import { useDebounce } from "@uidotdev/usehooks";
 import { useEffect, useState } from "react";
 
 export type Searchbar = SearchField &
-  Omit<useQueryParams, "searchParams" | "onChange"> & {
-    searchParams: URLSearchParams;
-    paramName?: string;
-  } & (
-    | ({ local?: false } & Pick<useQueryParams, "onChange">)
+  (
+    | (Pick<useQueryParams, "onChange" | "pathname"> & {
+        local?: false;
+        searchParams: URLSearchParams;
+        paramName?: string;
+      })
     | {
         local: true;
         onChange: (search?: string) => void;
       }
   );
 
-export function Searchbar({
-  local,
-  onChange,
-  pathname,
-  searchParams,
-  paramName = "search",
-  ...props
-}: Searchbar) {
-  const defaultValue = !local ? searchParams.get(paramName) : null;
+export function Searchbar(props: Searchbar) {
+  let componentProps = {};
+
+  if (props.local) {
+    const { local, onChange, ...searchFieldProps } = props;
+    componentProps = searchFieldProps;
+  } else {
+    const {
+      local,
+      onChange,
+      pathname,
+      searchParams,
+      paramName,
+      ...searchFieldProps
+    } = props;
+    componentProps = searchFieldProps;
+  }
+
+  const defaultValue = !props.local
+    ? props.searchParams.get(props.paramName ?? "search")
+    : null;
 
   const [value, setValue] = useState(defaultValue);
   const search = useDebounce(value?.trim(), 150);
 
   const { setQueryParams } = useQueryParams({
-    onChange,
-    pathname,
-    searchParams: searchParams.toString(),
+    onChange: props.onChange,
+    pathname: props.local ? "" : props.pathname,
+    searchParams: props.local ? "" : props.searchParams.toString(),
   });
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: need to push new url only when search changes
   useEffect(() => {
-    if (local) onChange(search);
-    else if (search) setQueryParams({ search });
-    else onChange(pathname);
+    if (props.local) props.onChange(search);
+    else setQueryParams({ search });
   }, [search]);
 
   return (
