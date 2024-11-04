a5b8405f806c4871735c20c0a78aca7d04b91efe
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-11-04 17:02:00 +0530

    Update index.ts

--- packages/react/src/components/filters/index.ts
+++ packages/react/src/components/filters/index.ts
@@ -2,11 +2,8 @@ import { BooleanFilter } from "./BooleanFilter";
 import { ReferenceFilter } from "./ReferenceFilter";
 import { SelectFilter } from "./SelectFilter";
 
-export { BooleanFilter } from "./BooleanFilter";
 export { FiltersPanel } from "./FiltersPanel";
 export { FiltersPanelToggleButton } from "./FiltersPanelToggleButton";
-export { ReferenceFilter } from "./ReferenceFilter";
-export { SelectFilter } from "./SelectFilter";
 
 export const FILTER_COMPONENTS = {
   boolean: BooleanFilter,
